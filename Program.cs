e2964e594f9125dfa255460c67f94de6106720fc
Richard <Richard@DESKTOP-22VVORS>
2024-11-14 10:20:24 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Assignment_1_3_3
+{
+    internal class Program
+    {
+        //Write a program to read N number of values in an array and display in reverse order.
+        //Storing 3 elements?
+        //
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Please input the how many elements to store in array: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine($"Input {n} number of elements in the array :");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Element - {i} : ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("The values stored into the array are:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("The values stored into the array in reverse are:");
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
